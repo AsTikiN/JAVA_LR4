2f868f11322c2173967f1fe3a6ff70f2ae41264a
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2022-10-24 13:50:11 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\uD83C\uDF46 \uD83E\uDD5B \uD83C\uDF89");
+
+        String number1 = "5.3";
+        String number2 = "3.3";
+
+        float result  = Float.parseFloat(number1) + Float.parseFloat(number2);
+
+        System.out.println(result);
+    }
+}
